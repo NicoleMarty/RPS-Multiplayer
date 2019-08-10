0c8a8597a23b551c386d0755c5043e074b0bd198
Nicole Marty <nmarty310@gmail.com>
2019-08-10 18:07:59 -0500

    index file didn't add? starting on outlining code

---
+++ assets/javascript/javascript.js
@@ -0,0 +1,49 @@
+var playerArray = ["player1", "player2"];
+var player1Result;
+var player2Result;
+player1Wins;
+player2Wins;
+player1Losses;
+player2Losses;
+playerTie;
+
+
+$("#rock").on("click", function() {
+
+});
+
+$("#paper").on("click", function() {
+
+});
+
+$("#scissors").on("click", function() {
+
+});
+
+// for player1
+// if (win)
+{
+    player1Wins++;
+}
+// if (lose)
+{
+    player1Losses++;
+}
+// if (tie)
+{
+    return;
+}
+
+// for player2
+// if (win)
+{
+    player2Wins++;
+}
+// else if (lose)
+{
+    player2Losses++;
+}
+// if (tie)
+{
+    return;
+}
